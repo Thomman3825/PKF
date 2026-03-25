8f8418f4bd72d19a30015bf1aad61b3a375926af
Thomas <tkurian2@gmail.com>
2026-03-25 19:41:02 +0530

    change home

--- philipkuttys-farm/src/components/ContactForm.jsx
+++ philipkuttys-farm/src/components/ContactForm.jsx
@@ -17,7 +17,7 @@ export default function ContactForm() {
     const body = encodeURIComponent(
       `Name: ${data.name}\nEmail: ${data.email}${data.phone ? `\nPhone: ${data.phone}` : ''}\n\n${data.message}`
     )
-    window.location.href = `mailto:mail@philipkuttysfarm.com?subject=${subject}&body=${body}`
+    window.location.href = `mailto:tkurian2@gmail.com?subject=${subject}&body=${body}`
     setStatus('success')
     reset()
   }
