026df873cdc94acdd60881042dbb2b9ab2286880
Thomas <tkurian2@gmail.com>
2026-06-18 12:25:38 +0530

    Fix GitHub Pages routing: set base path, router basename, and 404 redirect

--- philipkuttys-farm/src/App.jsx
+++ philipkuttys-farm/src/App.jsx
@@ -45,7 +45,7 @@ function AnimatedRoutes() {
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/PKF">
       <ScrollToTop />
       <Navbar />
       <main>
